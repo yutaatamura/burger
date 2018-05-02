775fe6e62108d85c77ade9eb9b8c3245f8cf0f9e
Yuta Tamura <yutaatamura@gmail.com>
2018-05-02 17:05:47 -0500

    Add to server.js

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express(); 
 
 var PORT = process.env.PORT || 8080; 
 
-app.use(express.static(__dirname, '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-var exphbs = require("experss-handlebars");
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+app.get("/", function(req, res) {
+    res.send("hello");
+})
 
-var router = require("./controllers/burgers_controller.js");
-app.use('/', router);
+// var router = require("./controllers/burgers_controller.js");
+// app.use('/', router);
 
 
 app.listen(PORT, function() {
