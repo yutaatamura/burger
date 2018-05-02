cb57f2ff46c0b66f3cb4ff5666995b137bc092b7
Yuta Tamura <yutaatamura@gmail.com>
2018-05-02 16:11:54 -0500

    Add to orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,19 @@
-var connection = require('./connection.js');
+var connection = require('../config/connection.js');
 
+var orm = {
+    selectAll: function(whatToSelect, tableInput) {
+        var queryString = "SELECT ?? FROM ??";
+        connection.query(queryString, [whatToSelect, tableInput], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    insertOne: function(tableInput, cols, vals, cb) {
+        var queryString = "INSERT INTO ?? (??) VALUES (?)";
+        connection.query(queryString, [tableInput, cols, vals]), function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        }
+    },
+    updateOne: function(tableInput, )
+}
